8bc15fd511260a54f83bda19b4dfaa5893b24ab2
Dmitry Bogadukhov <bogadukhovd@gmail.com>
2023-01-29 01:03:06 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~> 0.12"
+  required_version = "~> 0.12" # В свежих версиях использовать >=
 }
 
 provider "yandex" {
