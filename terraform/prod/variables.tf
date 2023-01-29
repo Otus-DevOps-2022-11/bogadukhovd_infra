dfe52d107d7770bffbd3c0d1badbcd9f8fee9db8
Dmitry Bogadukhov <bogadukhovd@gmail.com>
2023-01-29 21:57:11 +0300

    terraform-2

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -17,12 +17,22 @@ variable "private_key_path" {
   # Описание переменной
   description = "Path to the private key used for ssh access"
 }
-variable "image_id" {
-  description = "Disk image"
-}
 variable "subnet_id" {
   description = "Subnet"
 }
 variable "service_account_key_file" {
   description = "key .json"
+}
+variable "instance_count" {
+  description = "Instance counts"
+
+  default = 1
+}
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
