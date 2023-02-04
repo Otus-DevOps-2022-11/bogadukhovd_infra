781ec87229683e5a7fb6083df876b87fe3094538
Dmitry Bogadukhov <bogadukhovd@gmail.com>
2023-02-04 21:52:18 +0300

    ansible-3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,10 +1,26 @@
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
 }
 
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.app-network.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+
+data "yandex_vpc_network" "vpc" {
+  name = "default"
+}
+resource "yandex_vpc_security_group" "nsg" {
+  name        = "app security group"
+  description = "description for app security group"
+  network_id  = data.yandex_vpc_network.vpc.id
+}
+resource "yandex_vpc_security_group_rule" "rule80" {
+  security_group_binding = yandex_vpc_security_group.nsg.id
+  direction              = "ingress"
+  description            = "port-80"
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+  port                   = 80
+  protocol               = "TCP"
 }
